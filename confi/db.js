9bf48ef88683eb22eeaf1b7b9c29ee9e8d9d14d0
philemon <kazahphilemon@gmail.com>
2023-07-22 08:25:28 +0100

    change the url

--- confi/db.js
+++ confi/db.js
@@ -2,7 +2,7 @@ const  mongoose  = require("mongoose");
 
 const connectDb = async () =>{
     try {
-        const conn =  await mongoose.connect("mongodb://127.0.0.1:27017/tutorial", {
+        const conn =  await mongoose.connect(process.env.MONGO_URL, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
                 // useCreateIndex: true
